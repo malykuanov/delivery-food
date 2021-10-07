0cc137d59354b03bed32607580e956b991ec59d4
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-07 23:07:20 +0300

    Set config.py and .env

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+import datetime
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ['FLASK_SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=100)
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_ECHO = True
