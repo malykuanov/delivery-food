68e1aaaba8b854c6a4f2db2b773e7c55981d9c22
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-09 22:28:41 +0300

    Add two product models

---
+++ app/products/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from app import db
+from flask_login import UserMixin
+
+
+class Product(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    description = db.Column(db.String(500))
+    category_id = db.Column(db.Integer, db.ForeignKey('ProductCategory.id'))
+    price = db.Column(db.Numeric(5, 2))
+
+    def __str__(self):
+        return f"id={self.id}, name={self.name}, price={self.price}"
+
+
+class ProductCategory(db.Model):
+    __tablename__ = 'product_category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(100), unique=True, nullable=False)
+
+    product = db.relationship('Product', backref='ProductCategory', uselist=False)
+
+    def __str__(self):
+        return f"id={self.id}, category={self.category}"
