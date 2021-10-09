af9bf735301b8987879f161ed03f3d63f520641b
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-09 22:03:17 +0300

    Add Flask-Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 from config import DevelopmentConfig
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+bootstrap = Bootstrap()
 
 login_manager = LoginManager()
 login_manager.login_view = '.login'
@@ -20,5 +22,6 @@ def create_app():
     app.config.from_object(DevelopmentConfig)
     db.init_app(app)
     migrate.init_app(app, db)
+    bootstrap.init_app(app)
     #login_manager.init_app(app)
     return app
--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template, url_for
 
-home = Blueprint('home', __name__, template_folder="templates", static_folder="static")
+home = Blueprint(
+    'home',
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path='/home-static'
+)
 
 
 @home.route("/")
 def index():
-    return render_template('home/home.html', title='Delivery Food')
+    return render_template('home/home.html')
