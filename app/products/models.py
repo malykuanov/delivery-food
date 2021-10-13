395529b1e708d1d57d71f33c02af23a2cc016d7d
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-13 21:23:38 +0300

    Add field composition in product_model

--- app/products/models.py
+++ app/products/models.py
@@ -7,6 +7,7 @@ class Product(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True, nullable=False)
     description = db.Column(db.String(500))
+    composition = db.Column(db.String(500))
     category_id = db.Column(db.Integer, db.ForeignKey('product_category.id'))
     price = db.Column(db.Numeric(5, 2))
 
@@ -24,4 +25,4 @@ class ProductCategory(db.Model):
     product = db.relationship('Product', backref='product_category', uselist=False)
 
     def __str__(self):
-        return f"id={self.id}, category={self.category}"
+        return f"{self.category}"
