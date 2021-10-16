56ce7b8da1d418aa6a8ee8ad2e90920ecf898361
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-16 17:12:33 +0300

    Add html page for product info

--- app/products/views.py
+++ app/products/views.py
@@ -31,8 +31,10 @@ def category_product(product_category, product):
     if product_category not in [category.slug for category in categories]:
         abort(404)
     products = Product.query.filter(Product.product_category.has(slug=product_category)).all()
-    if product not in [slugify(pr.name) for pr in products]:
+    if product not in [product.slug for product in products]:
         abort(404)
+    product = Product.query.filter_by(slug=product).first()
     return render_template('products/product.html',
                            categories=categories,
-                           product_category=product_category)
+                           product_category=product_category,
+                           product=product)
