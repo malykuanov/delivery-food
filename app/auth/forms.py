45d1f01e4b59f52e96c80d10738aac683c9db3b2
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-17 21:27:52 +0300

    Add registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -10,3 +10,21 @@ class LoginForm(FlaskForm):
                                     Length(min=4, max=100, message="Пароль должен быть от 4 до 100 символов")])
     remember = BooleanField("Remember? ", default=False)
     submit = SubmitField("Enter")
+
+
+class RegisterForm(FlaskForm):
+    name = StringField(
+        "Name: ",
+        validators=[DataRequired(), Length(min=3, max=50, message="Имя должно быть от 3 до 50 символов")]
+    )
+    address = StringField("Address: ", validators=[Length(max=200, message="Длина адресса не более 200 символов")])
+    email = StringField("Email: ", validators=[DataRequired(), Email("Некоректный email")])
+    psw1 = PasswordField(
+        "Password: ",
+        validators=[DataRequired(), Length(min=4, max=100, message="Пароль должен быть от 4 до 100 символов")]
+    )
+    psw2 = PasswordField(
+        "Repeat password: ",
+        validators=[DataRequired(), EqualTo('psw1', message="Пароли не совпадают")]
+    )
+    submit = SubmitField("Регистрация")
