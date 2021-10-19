52fc41fe84a0f0221b6958f83ad8c585774f708c
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-19 22:11:00 +0300

    Add models Cart and CartProduct

--- app/auth/models.py
+++ app/auth/models.py
@@ -14,11 +14,36 @@ class Users(db.Model, UserMixin):
     role = db.Column(db.String(50), default='user')
     date_registration = db.Column(db.DateTime, default=datetime.utcnow)
 
+    cart = db.relationship('Cart', backref='users', uselist=False)
+
     def has_role(self, role):
         return role == self.role
 
     def __str__(self):
         return f"{self.email}"
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    cart_products = db.relationship('CartProduct',
+                                    foreign_keys='CartProduct.cart_id',
+                                    backref='cart',
+                                    lazy='dynamic',
+                                    cascade='all, delete-orphan')
+
+
+assoc_cart_products = db.Table("assoc_cart_products",
+                               db.Column("cart_product_id", db.Integer, db.ForeignKey("cart_product.id")),
+                               db.Column("product_id", db.Integer, db.ForeignKey("product.id")))
+
+
+class CartProduct(db.Model):
+    __tablename__ = 'cart_product'
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'))
+    products = db.relationship("Product", backref="cart_product", secondary=assoc_cart_products)
 
 
 def get_user(user_id):
