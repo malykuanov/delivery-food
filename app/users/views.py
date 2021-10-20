bf63f933eb352b76f47909e8e69541b1205f2783
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-20 21:37:10 +0300

    Change name blueprint and Add view cart

--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-cart = Blueprint(
-    'CartProducts',
+from app.auth.models import CartProduct
+from app.products.models import ProductCategory
+
+cartProduct = Blueprint(
+    'cartProducts',
     __name__,
     template_folder="templates",
     static_folder="static",
     static_url_path='/home-static'
 )
+
+
+@cartProduct.route("/cart", methods=['GET'])
+def cart():
+    return render_template('users/cart.html',
+                           categories=ProductCategory.get_all_categories(),
+                           price=CartProduct.get_price_and_count()['price'],
+                           count=CartProduct.get_price_and_count()['count'],
+                           products_in_cart=CartProduct.products_in_cart())
