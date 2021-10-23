6eca59018f39c768ac17ac2df1d8ab773707bc4b
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-23 17:47:33 +0300

    Add GET for all products in db

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
-from flask_restx import Api
+from flask import Blueprint, jsonify
+from flask_restx import Api, Resource
 
+from app.products.models import Product, ProductSchema
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 api = Api(api_bp,
@@ -8,4 +9,15 @@ api = Api(api_bp,
           title='Delivery_food API',
           description='API for the "Delivery_food" service',
           doc='/doc/')
+
+
+@api.route('/product')
+class Products(Resource):
+    """Get all products"""
+
+    def get(self):
+        query = Product.query.all()
+        product_schema = ProductSchema(many=True)
+        result = product_schema.dump(query)
+        return jsonify(result)
 
