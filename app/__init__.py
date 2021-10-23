2b417aa7f3e81be1f6624e8dbd5b9b7b902b2ebc
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-23 17:26:42 +0300

    Add marsmallow-sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-
+from flask_marshmallow import Marshmallow
 from config import DevelopmentConfig
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 bootstrap = Bootstrap()
-
 login_manager = LoginManager()
-
+ma = Marshmallow()
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
@@ -21,4 +20,5 @@ def create_app(config_class=DevelopmentConfig):
     migrate.init_app(app, db)
     bootstrap.init_app(app)
     login_manager.init_app(app)
+    ma.init_app(app)
     return app
